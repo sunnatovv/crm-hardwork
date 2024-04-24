5e1a5f4b47063012d66d1366081475faa7f8ada9
Samandar Sunnatov <sunnatovsamandar77@gmail.com>
2024-04-24 08:27:21 +0500

    question fixed

--- 2-question.js
+++ 2-question.js
@@ -1,40 +1,25 @@
-// 202. Happy Number
-
-class ListNode {
-  constructor(val = 0, next = null) {
-    this.val = val;
-    this.next = next;
-  }
-}
-
 class Solution {
-  mergeTwoLists(list1, list2) {
-    let ans = new ListNode();
-    let ptr = ans;
+  isHappy(n) {
+    let nString = n.toString();
+    return this.validate(nString, new Set());
+  }
 
-    while (list1 !== null && list2 !== null) {
-      if (list1.val <= list2.val) {
-        ans.next = new ListNode(list1.val);
-        list1 = list1.next;
-      } else {
-        ans.next = new ListNode(list2.val);
-        list2 = list2.next;
-      }
-      ans = ans.next;
+  validate(nString, prevNums) {
+    if (nString === '1') return true;
+
+    if (prevNums.has(nString)) return false;
+
+    prevNums.add(nString);
+
+    let newN = 0;
+
+    for (let num of nString.split('')) {
+      newN += num ** 2;
     }
 
-    while (list1 !== null) {
-      ans.next = new ListNode(list1.val);
-      list1 = list1.next;
-      ans = ans.next;
-    }
-
-    while (list2 !== null) {
-      ans.next = new ListNode(list2.val);
-      list2 = list2.next;
-      ans = ans.next;
-    }
-
-    return ptr.next;
+    return this.validate(newN.toString(), prevNums);
   }
 }
+
+const solution = new Solution();
+console.log(solution.isHappy(19));
